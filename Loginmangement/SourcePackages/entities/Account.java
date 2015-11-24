e46704ef73e0ee554e3596256a3e3a384df487b9
kalishavali <kalishavali.rgukt@gmail.com>
2015-11-25 01:26:40 +0530

    Create Account.java

---
+++ Loginmangement/SourcePackages/entities/Account.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class Account {
+    public String username;
+    private String password;
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setUsername(String username){
+        this.username=username;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public Account(String username,String password){
+        super();
+        this.username=username;
+        this.password=password;
+    }
+    public Account(){
+        super();
+    }
+}
