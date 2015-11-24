7c2de8b41e7fea2ddf58c669eabf9b42f68a7737
kalishavali <kalishavali.rgukt@gmail.com>
2015-11-25 01:25:46 +0530

    Create AccountModel.java

---
+++ Loginmangement/SourcePackages/Model/AccountModel.java
@@ -0,0 +1,7 @@
+package model;
+
+public class AccountModel {
+    public boolean login(String username,String password){
+        return username.equalsIgnoreCase("abc")&&password.equalsIgnoreCase("123");
+    }
+}
