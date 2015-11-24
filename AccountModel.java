6e45f3635855cd00f4701d242b921cba6b7d42a0
kalishavali <kalishavali.rgukt@gmail.com>
2015-11-25 01:32:44 +0530

    Delete AccountModel.java

--- AccountModel.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class AccountModel {
-    public boolean login(String username,String password){
-        return username.equalsIgnoreCase("abc")&& password.equalsIgnoreCase("123");
-    }
-}
