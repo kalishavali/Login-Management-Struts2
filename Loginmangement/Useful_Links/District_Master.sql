fd290acd1fecee0e504ecf9067e4ec6c5674179c
kalishavali <kalishavali.rgukt@gmail.com>
2016-02-26 11:01:40 +0530

    Added files via upload

---
+++ Loginmangement/Useful_Links/District_Master.sql
@@ -0,0 +1,75 @@
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('00', '_At State Level', 'A', '01', 'Y', '', 'B', '4', '_At State Level', null, '01', 'N', 'APST', 'Zone-I', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('00', '_At State Level', 'T', '02', 'Y', '', 'B', '4', '_At State Level', null, '01', 'N', 'TNST', 'Zone-I', 'MutiZone-I', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('03', 'Visakhapatnam', 'A', '01', 'Y', '£•πP∂pdÈA', 'B ', '3', 'Visakhapatnam', null, '02', 'N', 'VSP', 'Zone-I', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('04', 'East Godavari', 'A', '01', 'Y', 'hµ√±µ¿Í S–l∏∂¢±º', 'B ', '2', '  East Godavari', null, '02', 'N', 'EG', 'Zone-II', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('05', 'West Godavari', 'A', '01', 'Y', 'p¶€∂¢¿ S–l∏∂¢±º', 'B ', '3', 'West Godavari', null, '02', 'N', 'WG', 'Zone-II', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('06', 'Krishna', 'A', '01', 'Y', 'O∫Ò∂®‰ ', 'B ', '3', 'Krishna', null, '02', 'N', 'KRI', 'Zone-II', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('07', 'Guntur', 'A', '01', 'Y', 'Sµ¿Ad√±µ¿', 'B ', '2', '  Guntur', null, '03', 'N', 'GNT', 'Zone-III', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('08', 'Prakasam', 'A', '01', 'Y', '∂pÒO∏∂•A ', 'B ', '2', '  Prakasam', null, '03', 'N', 'PKSM', 'Zone-III', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('09', 'S.P.S Nellore', 'A', '01', 'Y', '»mv√Ù±≥ ', 'B ', '2', '  Nellore', null, '03', 'N', 'NLR', 'Zone-III', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('10', 'Chittoor', 'R', '01', 'Y', 'Whµ√Â±≥ ', 'B ', '1', '   Chittoor', null, '03', 'N', 'CTR', 'Zone-IV', 'MutiZone-II', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('11', 'Kadapa', 'R', '01', 'Y', 'Oµfµ∂p ', 'B ', '1', '   Cuddaph', null, '03', 'N', 'KDP', 'Zone-IV', 'MutiZone-II', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('12', 'Anantapur', 'R', '01', 'Y', 'C∂mAhµ∂p¡±µA ', 'B ', '1', '   Anantapur', null, '03', 'N', 'ATP', 'Zone-IV', 'MutiZone-II', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('13', 'Kurnool', 'R', '01', 'Y', 'Oµ±µ√Èv≥ ', 'B ', '2', '  Kurnool', null, '03', 'N', 'KNL', 'Zone-IV', 'MutiZone-II', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('14', 'Mahabubnagar', 'T', '02', 'Y', '∂¢¿∂¨s√s≥∂mSµ±≥ ', 'B ', '1', '   Mahabubnagar', null, '01', 'N', 'MBNR', 'Zone-VI', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('15', 'Ranga Reddy', 'T', '02', 'Y', '±µAS∏±«f∫‚ ', 'B ', '1', '   Ranga Reddy', null, '01', 'N', 'RR', 'Zone-VI', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('17', 'Medak', 'T', '02', 'Y', '»¢¿lµO≥ ', 'B ', '1', '   Medak', null, '01', 'N', 'MDK', 'Zone-VI', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('18', 'Nizamabad', 'T', '02', 'Y', 'nYπ∂¢√sπl≥ ', 'B ', '1', '   Nizamabad', null, '01', 'Y', 'NZB', 'Zone-VI', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('19', 'Adilabad', 'T', '02', 'Y', 'Clºvπsπl≥ ', 'B ', '1', '   Adilabad', null, '01', 'N', 'ADB', 'Zone-V', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('20', 'Karimnagar', 'T', '02', 'Y', 'Oµ±øA∂mSµ±≥ ', 'B ', '1', '   Karimnagar', null, '01', 'N', 'KMNR', 'Zone-V', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('21', 'Warangal', 'T', '02', 'Y', '∂¢±µASµv≥ ', 'B ', '1', '   Warangal', null, '01', 'N', 'WGL', 'Zone-V', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('22', 'Khammam', 'T', '02', 'Y', 'P∂¢¿ÓA ', 'B ', '1', '   Khammam', null, '02', 'N', 'KMM', 'Zone-V', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('23', 'Nalgonda', 'T', '02', 'Y', '∂mvŒÿAfµ ', 'B ', '1', '   Nalgonda', null, '01', 'N', 'NLG', 'Zone-VI', 'MutiZone-III', 'TG');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('01', 'Srikakulam', 'A', '01', 'Y', 'ßÒO∏Oµ¿yµA', 'B ', '2', '  Srikakulam', null, '02', 'N', 'SKLM', 'Zone-I', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('02', 'Vizianagaram', 'A', '01', 'Y', '£Y±¿µ¿∂mSµ±µA ', 'B ', '1', '   Vizianagaram', null, '02', 'N', 'VZM', 'Zone-I', 'MutiZone-I', 'AP');
+
+insert into new_District_Master (DISTRICT_ID, DISTRICT_DESCRIPTION, REGION_CODE, STATE_CODE, FLAG, TELUGU_NAME, PROGRAM_TYPE, PHASE, OLD_NAME, NEW_WORKPROGRESS_DATE, REGION_ID, IS_BGWSS, DNAME_SHORT, ZONE_NAME, MULTI_ZONE, STATE)
+values ('16', 'Hyderabad', 'T', '02', 'N', '', 'B', '', '', null, '01', 'N', 'HYD', '', '', 'TG');
+
